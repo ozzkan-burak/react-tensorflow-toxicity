6435eea06202d3b76c7ee3385fc1542a0e54fe57
ozzkan-burak <brk.ozzkan@gmail.com>
2023-03-19 18:55:47 +0300

    episode1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+require('@tensorflow/tfjs')
+const toxicity = require("@tensorflow-models/toxicity");
 
 function App() {
+
+  const textRef = useRef();
+
+  const onValidate = async () => {
+    
+    const treshold = 0.9;
+    const result = await toxicity.load(treshold);
+    const response =await result.classify(textRef.current.value);
+    console.log('Dönüş değeri', response);
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        TensorFlow text detect
+        <textarea ref={textRef}>
+
+        </textarea>
       </header>
+      <button onClick={()=> onValidate()}>Tıkla ve kotrol et</button>
     </div>
   );
 }
